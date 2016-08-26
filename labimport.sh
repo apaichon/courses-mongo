133f650de271c49271f0022e60db8b45ff397d79
apaichon <apaichon@hotmail.com>
2016-08-26 14:13:28 +0700

    labimport
    import data script

---
+++ labimport.sh
@@ -0,0 +1,5 @@
+./mongoimport --db test --collection customers --file customers.json
+./mongoimport --db test --collection customerAddresses --file customerAddresses.json
+./mongoimport --db test --collection places --file places.json
+./mongoimport --db test --collection airports --file airports.json
+./mongoimport --db test --collection saleBooks --file saleBooks.json
